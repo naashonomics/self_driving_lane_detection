8d4d31cf8bac2139f10b7d12929ac627243f17b5
Avinash Anantharamu <anantharamu.avinash@yahoo.com>
2018-09-06 14:43:07 -0700

    Add files via upload

---
+++ lanes.py
@@ -0,0 +1,73 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+def average_slope_intercept(image,lines):
+    left_fit=[]
+    right_fit=[]
+    for line in lines:
+        x1,y1,x2,y2=line.reshape(4)
+        parameters = np.polyfit((x1,x2),(y1,y2),1)
+        slope=parameters[0]
+        intercept=parameters[1]
+        if slope <0:
+            left_fit.append(slope,intercept)
+        else:
+            right_fit.append(slope,intercept)
+        print(left_fit)
+        print(right_fit)
+def canny(image):
+    #convert color to gray image
+    """
+    Edge Detection technique : Identify sharp change in intnsity in adjacent pixels
+    Image is an array of pixels
+    Step1: Convert colored image to Gray Scale image
+    """
+    grey=cv2.cvtColor(image,cv2.COLOR_RGB2GRAY)
+    """
+    Step2 : Reduce Noise using Gaussian Blur
+    """
+    blur = cv2.GaussianBlur(grey,(5,5),0)
+    """
+    Step3: Apply Canny Edge Detection
+    derivative(f(x,y))
+    """
+    canny_edge=cv2.Canny(blur,50,150)
+    return canny_edge
+
+def display_lines(image,lines):
+    line_image=np.zeros_like(image)
+    if lines is not None:
+        for line in lines:
+            #unpack
+            x1,y1,x2,y2=line.reshape(4)
+            cv2.line(line_image,(x1,y1),(x2,y2),(255,0,0,15))
+    return line_image
+"""
+Step 4 Region of Interest
+"""
+def region_of_interest(image):
+    height=image.shape[0]
+    polygons = np.array([
+    [(200,height),(1100,height) ,(550,250)]
+    ])
+    mask=np.zeros_like(image)
+    cv2.fillPoly(mask,polygons,255)
+    """
+    Step 5 Bitwise AND
+    """
+    masked_image=cv2.bitwise_and(image,mask)
+    return masked_image
+image =cv2.imread('test_image.jpg')
+lane_image=np.copy(image)
+canny_image=canny(lane_image)
+cropped_image=region_of_interest(canny_image)
+"""
+Step 6 : Hough Transform
+"""
+lines=cv2.HoughLinesP(cropped_image, 2, (np.pi/180) ,100, np.array([]), minLineLength=40, maxLineGap=5)
+average_lines=average_slope_intercept(lane_image,lines)
+line_image=display_lines(lane_image,lines)
+combo_image=cv2.addWeighted(lane_image,0.8,line_image,1,3)
+cv2.imshow("result",combo_image)
+cv2.waitKey(0)
