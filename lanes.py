cec9f2a99c488f4e4d98aa15b79a1d1460847827
Avinash Anantharamu <anantharamu.avinash@yahoo.com>
2018-09-06 15:38:54 -0700

    Add files via upload

--- lanes.py
+++ lanes.py
@@ -1,21 +1,6 @@
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
-
-def average_slope_intercept(image,lines):
-    left_fit=[]
-    right_fit=[]
-    for line in lines:
-        x1,y1,x2,y2=line.reshape(4)
-        parameters = np.polyfit((x1,x2),(y1,y2),1)
-        slope=parameters[0]
-        intercept=parameters[1]
-        if slope <0:
-            left_fit.append(slope,intercept)
-        else:
-            right_fit.append(slope,intercept)
-        print(left_fit)
-        print(right_fit)
 def canny(image):
     #convert color to gray image
     """
@@ -23,7 +8,7 @@ def canny(image):
     Image is an array of pixels
     Step1: Convert colored image to Gray Scale image
     """
-    grey=cv2.cvtColor(image,cv2.COLOR_RGB2GRAY)
+    grey=cv2.cvtColor(lane_image,cv2.COLOR_RGB2GRAY)
     """
     Step2 : Reduce Noise using Gaussian Blur
     """
@@ -60,14 +45,13 @@ def region_of_interest(image):
     return masked_image
 image =cv2.imread('test_image.jpg')
 lane_image=np.copy(image)
-canny_image=canny(lane_image)
-cropped_image=region_of_interest(canny_image)
+canny_edge=canny(lane_image)
+cropped_image=region_of_interest(canny_edge)
 """
 Step 6 : Hough Transform
 """
 lines=cv2.HoughLinesP(cropped_image, 2, (np.pi/180) ,100, np.array([]), minLineLength=40, maxLineGap=5)
-average_lines=average_slope_intercept(lane_image,lines)
 line_image=display_lines(lane_image,lines)
-combo_image=cv2.addWeighted(lane_image,0.8,line_image,1,3)
+combo_image=cv2.addWeighted(lane_image,0.8,line_image,1,0)
 cv2.imshow("result",combo_image)
 cv2.waitKey(0)
